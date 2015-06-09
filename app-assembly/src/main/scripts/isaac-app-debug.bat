c328b7dfeb23d8dd2a4a2fecec71babc34eda46b
darmbrust <daniel.armbrust.list@gmail.com>
2015-06-08 22:00:26 -0500

    switch garbage collectors, bump up the metaspace garbage collection trigger which was causing long execution pauses

--- app-assembly/src/main/scripts/isaac-app-debug.bat
+++ app-assembly/src/main/scripts/isaac-app-debug.bat
@@ -1,5 +1,7 @@
 java ^
   -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000 ^
-  -Xms2000M ^
+  -Xms3000M ^
   -Xmx6000M ^
+  -XX:+UseG1GC ^
+  -XX:MetaspaceSize=100M ^
   -cp "lib/*" gov.va.isaac.gui.App
--- app-assembly/src/main/scripts/isaac-app.bat
+++ app-assembly/src/main/scripts/isaac-app.bat
@@ -1,4 +1,6 @@
 java ^
-  -Xms2000M ^
+  -Xms3000M ^
   -Xmx6000M ^
+  -XX:+UseG1GC ^
+  -XX:MetaspaceSize=100M ^
   -cp "lib/*" gov.va.isaac.gui.App
